519b0bd77a15603bf091bb5fd3012ff4d9f76326
Kachii <Kacheokcheong@gmail.com>
2021-04-13 22:15:22 +0100

    restart tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './App.css';
-import Modal from './components/modal'
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
 
   return (
-    <>
-      {showModal && (
-        <Modal content="hello world"
-          onCloseButtonClick={() => setShowModal(false)}
-        />
-      )}
-      <div className="App">
-        <header className="App-header">
-          <button onClick={() => {setShowModal(true);}}>
-            Show Modal
-          </button>
-        </header>
-      </div>
-    </>
+    <h1>Redux!</h1>
   );
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
-import allReducers from './reducers'
-import {Provider} from 'react-redux'
 
-
-let store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
-);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals();
