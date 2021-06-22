82a455368b59a5d2f573b8fe7e25aa8cedc3d9f9
Kachii <Kacheokcheong@gmail.com>
2021-06-22 16:12:59 +0100

    added redux modal

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import logo from './logo.svg';
+import './App.css';
+import Modal from './components/Modal';
+import { showModal } from './store/actions';
+
+const mapDispatchToProps = {
+  dispatchShowModal: showModal,
+};
+
+const connector = connect(undefined, mapDispatchToProps);
+
+type AppProps = {} & ConnectedProps<typeof connector>;
+
+function App(props: AppProps) {
+  const { dispatchShowModal } = props;
+
+  return (
+    <>
+      <Modal />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button
+            onClick={() => {
+              dispatchShowModal({
+                title: 'A new title.',
+                description: 'And a new description too.',
+                onButtonClick: (event: React.MouseEvent) => {
+                  alert('You clicked that button!');
+                },
+              });
+            }}
+          >
+            Show Modal
+          </button>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default connector(App);
---
+++ src/interfaces/modal-properties.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface ModalProperties {
+  title: string;
+  description: string;
+  onButtonClick: (event: React.MouseEvent) => void;
+}
+
+export default ModalProperties;
